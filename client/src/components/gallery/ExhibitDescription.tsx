7cfa22775293e53dca1211bad82ae8c99c3d783a
Dmitry Markelov <dmarkelo89@gmail.com>
2025-04-27 21:32:49 +0000

    fixed bugs

--- client/src/components/gallery/ExhibitDescription.tsx
+++
@@ -1,7 +0,0 @@
-import { usePortfolio } from '../../lib/stores/usePortfolio';
-
-// Component has been simplified to not show descriptions as requested
-export default function ExhibitDescription() {
-  // Return null to render nothing - descriptions have been removed
-  return null;
-}
